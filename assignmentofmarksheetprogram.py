59f0e92897dbf4e72f26e972eb404c68b1f107d5
ummi12345 <ummi.hussain2924@gmail.com>
2019-05-26 23:11:48 +0500

    Update assignmentofmarksheetprogram.py

--- assignmentofmarksheetprogram.py
+++ assignmentofmarksheetprogram.py
@@ -7,7 +7,9 @@ sub5=int(input("islamiat marks:"))
 sum = sub1+sub2+sub3+sub4+sub5
 avg= sum/425*100
 print(avg)
-if avg >= 80:
+if avg >= 100:
+    print("error in entering the marks,please try again")
+elif avg >= 80 and avg < 100:
     print("Grade: A+")
 elif avg >= 70 and avg < 80:
     print("Grade: A")
